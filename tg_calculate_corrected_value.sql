cdf19ec858b48b2f060604da656a4c5b348d7ef4
Gerhard <29224009+GZEI@users.noreply.github.com>
2018-11-29 12:05:40 +0100

    Update tg_calculate_corrected_value.sql

--- tg_calculate_corrected_value.sql
+++ tg_calculate_corrected_value.sql
@@ -6,10 +6,11 @@ BEGIN
 	SET NOCOUNT ON;
     DECLARE @sensor int;
     DECLARE @value_corrected float;
+	DECLARE @value_corrected_from_sensor float;
     DECLARE @newtimestamp DATETIMEOFFSET;
     DECLARE @value_orig float;
-    SELECT TOP 1 @sensor = sensor_id, @value_orig = value_orig, @newtimestamp = measure_time FROM inserted;
-	IF ((SELECT correction_function FROM dbo.sensor WHERE sensor_ID = @sensor) IS NOT NULL)
+    SELECT TOP 1 @sensor = sensor_id, @value_orig = value_orig, @newtimestamp = measure_time, @value_corrected_from_sensor = value_corrected FROM inserted;
+	IF ((SELECT correction_function FROM dbo.sensor WHERE sensor_ID = @sensor) IS NOT NULL AND @value_corrected_from_sensor IS NULL)
 	BEGIN 
 		DECLARE @function NVARCHAR(MAX) = (SELECT 'SELECT @value_corrected=' + REPLACE((SELECT correction_function FROM dbo.sensor WHERE sensor_ID = @sensor),'x',@value_orig) +';');
 		DECLARE @parm NVARCHAR(50) = '@value_corrected float output'
