d55c943baf4932e79ed1e37e092a81a1d1ccd19e
mipo17 <45309101+mipo17@users.noreply.github.com>
2018-12-01 11:57:56 +0100

    add error hanling

--- sp_rekord_werte.sql
+++ sp_rekord_werte.sql
@@ -30,27 +30,54 @@ FROM dbo.user_permission
 -- Sensor muss gültig sein, sonst Fehler
 
 --Ergebnisrückgabe mittels SELECT --> einfach für Frontend
+-- datum als String übergeben, da sonst ein falsches datum nicht überprueft werden kann, weil datumsuepruefung auf selben Level wie Try und Catch ist!
+--Date input as string, because if the input is datatype 'date' the error level in case of a wrong date is not in range between 10-19 thus cannot be handled in trycatch block!
 GO
 ALTER PROCEDURE dbo.sp_rekord_werte
-    @sensor_id INT = NULL,
-    @von_datum char(8) = NULL,
-    @bis_datum char(8) = NULL,
-    @separate_messwerte BIT = NULL
+    @subscriber_id INT,
+    @sensor_id INT,
+    @von_datum char(8),
+    @bis_datum char(8),
+    @separate_messwerte BIT
 AS
 BEGIN
 
     SET NOCOUNT ON;
     
+    IF (SELECT COUNT(*) 
+        FROM dbo.user_permission  
+        WHERE @subscriber_id = subscriber_ID) = 0 --check if subscriber has permition to subscribe a sensor
+    BEGIN
+        SELECT 50003 AS Fehlernummer, CONCAT('Subscriber hat keine Zugriffsrechte auf Sensor :',CONVERT(INT,@sensor_id)) AS Fehlermeldung; 
+        RETURN
+    END
+    ELSE IF (SELECT COUNT(*) 
+            FROM dbo.user_permission 
+            WHERE 1 = subscriber_ID AND (GETDATE() BETWEEN valid_from AND valid_to OR valid_to IS NULL))=0--CHECK if permition is still valid
+    BEGIN
+        SELECT 50004 AS Fehlernummer,CONCAT('Subscriber Zugriffsrecht abgelaufen fuer Sensor :', CONVERT(INT,@sensor_id)) AS Fehlermeldung; 
+        RETURN
+    END
+
+    IF ( SELECT COUNT(*)
+            FROM dbo.sensor sen
+            INNER  JOIN dbo.measurement meas ON meas.sensor_ID = sen.sensor_ID
+            WHERE (measure_time BETWEEN @von_datum AND @bis_datum) AND sen.sensor_ID = @sensor_id) = 0
+    BEGIN
+         SELECT 50004 AS Fehlernummer,CONCAT('Keine Messwerte vorhanden fuer Sensor:', @sensor_id,' zwischen ',@von_datum, ' und ',@bis_datum) AS Fehlermeldung; 
+        RETURN
+    END 
+   
     
     BEGIN TRY
         IF TRY_CONVERT(DATE,@von_datum) IS NULL
         BEGIN
-        SELECT 'von Datum falsch' AS Result;   
+        SELECT 50001 AS Fehlernummer, 'von Datum falsch' AS Fehlermeldung; 
         RETURN
         END
         ELSE IF TRY_CONVERT(DATE,@bis_datum) IS NULL
         BEGIN
-        SELECT 'bis Datum falsch' AS Result;   
+        SELECT 50002 AS Fehlernummer, 'bis Datum falsch' AS Fehlermeldung; 
         RETURN
         END
         ELSE
@@ -65,19 +92,19 @@ BEGIN
        
     END TRY
     BEGIN CATCH
-        PRINT 'jajaj'
+         SELECT ERROR_NUMBER() AS Fehlernummer, ERROR_MESSAGE() AS Fehlermeldung; -- default error
     END CATCH
-
-    IF MONTH(@von_datum)>12
-    BEGIN
-        SELECT *
-        FROM dbo.user_permission
-
-    END
-
     
 END
 
-EXEC dbo.sp_rekord_werte 1,'2018-1-20','2018-122-21',1
+EXEC dbo.sp_rekord_werte @subscriber_id = 1, @sensor_id = 1 ,@von_datum = '2018-1-20',@bis_datum = '2018-1-12',@separate_messwerte= 1
 
-SELECT TRY_CONVERT([date], '12/31/2010') AS Result;  
+SELECT TRY_CONVERT([date], '12/32/2010') AS Result;  
+
+SELECT COUNT(*) FROM dbo.user_permission sub WHERE 1 = sub.subscriber_ID
+
+SELECT GETDATE() as sss
+
+SELECT COUNT(*) FROM dbo.user_permission per WHERE 1 = per.subscriber_ID AND (GETDATE() BETWEEN per.valid_from AND per.valid_to OR per.valid_to IS NULL)-- OR (per.valid_from >= GETDATE() AND per.valid_to IS NULL)
+
+SELECT COUNT(*) FROM dbo.user_permission per WHERE 1 = per.subscriber_ID AND per.valid_to IS NULL
