909c474d00ee77d5298df7185df8ed186706748b
mipo17 <45309101+mipo17@users.noreply.github.com>
2019-01-02 16:06:38 +0100

    Update tg_channel_permission.sql
    new version

--- tg_channel_permission.sql
+++ tg_channel_permission.sql
@@ -9,35 +9,27 @@ BEGIN
     --check if the subscriber has the permition to read at least ONE Sensor:
     -- if no sensor readable, creating an channel won't be adviceable
     
-    IF(SELECT COUNT(*) FROM 
-    (SELECT up.subscriber_ID, up.sensor_ID, up.valid_from, up.valid_to
-    FROM dbo.user_permission up
-    INNER JOIN inserted ins ON up.subscriber_ID = ins.subscriber_ID) AS tb) = 0
-    BEGIN
-        ROLLBACK TRANSACTION;
-        THROW 50015, 'Subscriber hat keine Zugriffsrechte auf Sensor', 1;
-        RETURN
-    END
+    -- IF(SELECT COUNT(*) FROM 
+    -- (SELECT up.subscriber_ID, up.sensor_ID, up.valid_from, up.valid_to
+    -- FROM dbo.user_permission up
+    -- INNER JOIN inserted ins ON up.subscriber_ID = ins.subscriber_ID) AS tb) = 0
+    -- BEGIN
+    --     ROLLBACK TRANSACTION;
+    --     THROW 50015, 'Subscriber hat keine Zugriffsrechte auf Sensor', 1;
+    --     RETURN
+    -- END
+
+    SET NOCOUNT ON;
+	DECLARE @subscriber int;
+    	DECLARE @channel int;
+    	SELECT TOP 1 @subscriber = subscriber_ID, @channel = channel_ID FROM inserted;
+	IF ((SELECT count(*) FROM dbo.sensor_group sg left JOIN user_permission up ON sg.sensor_ID = up.sensor_ID WHERE sg.channel_id = @channel AND up.subscriber_ID = @subscriber) != (SELECT count(*) FROM dbo.sensor_group sg WHERE sg.channel_ID = @channel))
+	BEGIN
+		 ROLLBACK TRANSACTION;
+         THROW 50300, 'Subscriber has no permition on Sensor', 1;
+         RETURN
+	END
     
-    --IF UPDATE(ekpreis)--UPDATE sagt nur aus,ob spalte geändert wird. 
-    --UPDATE liefert bei INSERT TRUE!!
-    --BEGIN
-     --   INSERT INTO mipo.preise(artnr,preis, typ)
-     --   SELECT artnr, ekpreis, 'E'
-     --   FROM inserted
-    --END
-
-    --IF UPDATE(vkpreis) --UPDATE sagt nur aus,ob spalte geändert wird.
-    --BEGIN
-    --     INSERT INTO mipo.preise(artnr,preis, typ)
-    --    SELECT artnr, ekpreis, 'V'
-     --   FROM inserted
-    --END
+    
 END
-
- 
-    SELECT sg.channel_ID ,up.subscriber_ID, up.sensor_ID, up.valid_from, up.valid_to
-    FROM dbo.user_permission up
-    INNER JOIN dbo.sensor_group sg ON up.sensor_ID = sg.sensor_ID
-    INNER JOIN dbo.subscription sp ON sp.subscriber_ID = 1
 
