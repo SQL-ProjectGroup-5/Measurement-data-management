09afbdc71a1efbb71475de46fd698e31dda91034
mipo17 <45309101+mipo17@users.noreply.github.com>
2019-01-02 16:06:27 +0100

    Update sp_subscribe.sql
    bug fixes

--- sp_subscribe.sql
+++ sp_subscribe.sql
@@ -51,22 +51,30 @@ BEGIN
          SELECT 50102 AS ERRORNUMBER, 'Subscriber ID not available' AS ERRORMESSAGE; 
         RETURN
     END
+    --check if subscriber is linked to a channel, has to be checked otherwise an error will occur
+    IF (SELECT COUNT(*) FROM dbo.subscription WHERE (subscriber_ID = @subscriber_id AND channel_ID = @channel_ID)) = 0
+    BEGIN
+         SELECT 50103 AS ERRORNUMBER, 'Subscriber ID is not linked to any channel!' AS ERRORMESSAGE; 
+        RETURN
+    END
+    
     --check if data format and input is incorrect:
     IF TRY_CONVERT(DATETIME2,@valid_from_date) IS NULL
     BEGIN
-        SELECT 50103 AS ERRORNUMBER, 'wrong from-date format: yyyy-MM-dd hh:mm:ss +01:00' AS ERRORMESSAGE; 
+        SELECT 50104 AS ERRORNUMBER, 'wrong from-date format: yyyy-MM-dd hh:mm:ss +01:00' AS ERRORMESSAGE; 
         RETURN
     END
     ELSE IF TRY_CONVERT(DATETIME2,@valid_to_date) IS NULL
         BEGIN
-        SELECT 50104 AS ERRORNUMBER, 'wrong to-date format: yyyy-MM-dd hh:mm:ss +01:00' AS ERRORMESSAGE; 
+        SELECT 50105 AS ERRORNUMBER, 'wrong to-date format: yyyy-MM-dd hh:mm:ss +01:00' AS ERRORMESSAGE; 
     RETURN
     END
     ELSE IF @valid_from_date>@valid_to_date
     BEGIN
-        SELECT 50105 AS ERRORNUMBER, 'from-date greater than to-date' AS ERRORMESSAGE; 
+        SELECT 50106 AS ERRORNUMBER, 'from-date greater than to-date' AS ERRORMESSAGE; 
         RETURN
     END
+
 
     --temporary table to store string values from procedure parameter
     CREATE TABLE #tempSensorIDs 
@@ -128,8 +136,8 @@ BEGIN
             
             --testing purpose:
             SELECT * FROM dbo.subscription
-        COMMIT TRANSACTION
         END
+        COMMIT TRANSACTION
     END TRY
     BEGIN CATCH
         SELECT  ERROR_NUMBER() AS Fehlernummer, ERROR_MESSAGE() AS Fehlermeldung;  -- default error
@@ -137,14 +145,25 @@ BEGIN
 
 END
 
+--add a new channel with sensors
 EXEC dbo.sp_prj_subscribe @subscriber_id = 2, @valid_from_date = '2018-11-20 00:00:00 +01:00', @valid_to_date = '2018-12-15 23:59:00 +01:00', 
                           @channel_description = 'a new one', @channel_name='bla bla',@sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
 
+--update channel (update valid from and valid to date)
+EXEC dbo.sp_prj_subscribe @channel_id=1, @subscriber_id = 1, @valid_from_date = '2018-11-20 10:00:00 +01:00', @valid_to_date = '2018-12-15 23:59:00 +01:00', 
+                          @sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
+
+                          SELECT COUNT(*) FROM dbo.subscription WHERE (subscriber_ID = 3 AND channel_ID = 2)
+                          SELECT COUNT(*) 
+            FROM dbo.subscription
+            WHERE channel_id = 1 AND subscriber_ID = 1
+
 SELECT * FROM dbo.user_permission
 SELECT * FROM dbo.sensor_group
+SELECT * FROM dbo.channel
+SELECT * FROM dbo.subscription
 
 --later maybe, turn valid_from and valid_to into optional parameters....
 PRINT CONVERT(DATETIME2,GETDATE())AT TIME ZONE 'Central European Standard Time'
 
-DECLARE @list AS VARCHAR(233)
-SET @list = (SELECT value FROM STRING_SPLIT('Lorem ipsum dolor sit amet.', ';'))
+SELECT COUNT(*) FROM dbo.subscriber WHERE (subscriber_ID = 2)
