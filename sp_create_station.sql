e0543c28c11352ee18e43124020dfc851fca9471
GZEI <29224009+GZEI@users.noreply.github.com>
2019-01-02 11:22:26 +0100

    Added logging capabilities

--- sp_create_station.sql
+++ sp_create_station.sql
@@ -17,6 +17,7 @@ BEGIN
 				SELECT @location = location_ID FROM dbo.location WHERE coordinates.STDistance(GEOGRAPHY::Point(@lat, @long, 4326)) < 10;
 				INSERT INTO dbo.station (location_ID, name, description) VALUES (@location, @name, @station_desc);
 				SELECT 50200 AS ERRORCODE, 'Successful INFO: Already existing location reused' AS ERRORMESSAGE;
+				INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50200,'Successful INFO: Already existing location reused');
 				COMMIT;
 			END ELSE
 			BEGIN
@@ -24,26 +25,38 @@ BEGIN
 				SET @location = @@IDENTITY;
 				INSERT INTO dbo.station (location_ID, name, description) VALUES (@location, @name, @station_desc);
 				SELECT 50201 AS ERRORCODE, 'Successful INFO: New location created' AS ERRORMESSAGE;
+				INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50201,'Successful INFO: New location created');
 				COMMIT;
 			END
 		END ELSE IF @location IS NOT NULL
 		BEGIN
 			INSERT INTO dbo.station (location_ID, name,description) VALUES (@location, @name, @station_desc);
 			SELECT 50200 AS ERRORCODE, 'Successful INFO: Already existing location reused' AS ERRORMESSAGE;
+			INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50200,'Successful INFO: Already existing location reused');
 			COMMIT;
 		END ELSE
 		BEGIN
 			SELECT 50203 AS ERRORCODE, 'Invalid parameters' AS ERRORMESSAGE;
 			ROLLBACK;
+			INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50203,'Invalid parameters');
 		END
 	END TRY
 	BEGIN CATCH
 	ROLLBACK;
 		IF ERROR_NUMBER() = 547
-			SELECT 50204 AS ERRORCODE, 'Wrong location ID' AS ERRORMESSAGE; 
-		ELSE IF ERROR_NUMBER = 2627
+		BEGIN
+			SELECT 50204 AS ERRORCODE, 'Wrong location ID' AS ERRORMESSAGE;
+			INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50204,'Wrong location ID');
+		END
+		ELSE IF ERROR_NUMBER() = 2627
+		BEGIN
 			SELECT 50205 AS ERRORCODE, 'Station name has to be unique' AS ERRORMESSAGE;
+			INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50205,'Station name has to be unique');
+		END
 		ELSE
+		BEGIN
 			SELECT 50206 AS ERRORCODE, 'Unknown error' AS ERRORMESSAGE; 
+			INSERT INTO dbo.logging (causing_user, involved_procedure, resulting_code, resulting_message) VALUES (SUSER_NAME(),'sp_create_station',50206,'Unknown error');
+		END
 	END CATCH
 END
