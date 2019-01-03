69f8b0e2ba2abc71efcd2ff96ec86e15dce8c938
mipo17 <45309101+mipo17@users.noreply.github.com>
2019-01-03 10:37:06 +0100

    Update tg_channel_permission.sql
    added comments
    minor corrections

--- tg_channel_permission.sql
+++ tg_channel_permission.sql
@@ -5,31 +5,25 @@ AFTER INSERT, UPDATE
 AS
 BEGIN
 
-    --check if the subscriber has the permition to read at least ONE Sensor:
-    -- if no sensor readable, creating an channel won't be adviceable
-    
-    -- IF(SELECT COUNT(*) FROM 
-    -- (SELECT up.subscriber_ID, up.sensor_ID, up.valid_from, up.valid_to
-    -- FROM dbo.user_permission up
-    -- INNER JOIN inserted ins ON up.subscriber_ID = ins.subscriber_ID) AS tb) = 0
-    -- BEGIN
-    --     ROLLBACK TRANSACTION;
-    --     THROW 50015, 'Subscriber hat keine Zugriffsrechte auf Sensor', 1;
-    --     RETURN
-    -- END
 
     SET NOCOUNT ON;
 	DECLARE @subscriber int;
     	DECLARE @channel int;
     	SELECT TOP 1 @subscriber = subscriber_ID, @channel = channel_ID FROM inserted;
+    
+    --compare the amount of sensors with permission to the amount of sensors linked to the subscribed channel
+    --a difference between means the subscriber is not allowed to read all sensors.
 	IF ((SELECT count(*) FROM dbo.sensor_group sg left JOIN user_permission up ON sg.sensor_ID = up.sensor_ID 
         WHERE sg.channel_id = @channel AND up.subscriber_ID = @subscriber) 
         != (SELECT count(*) FROM dbo.sensor_group sg WHERE sg.channel_ID = @channel))
 	BEGIN
 		 ROLLBACK TRANSACTION;
-         THROW 50300, 'Subscriber has no permition on sensor', 1;
+         THROW 50300, 'Subscriber has no permition to read all sensors linked to the channel', 1;
          RETURN
 	END
     
-    
 END
+
+
+
+
