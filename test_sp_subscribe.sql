426cb0f8c1ac87c088322c56faa311a76e269cc7
mipo17 <45309101+mipo17@users.noreply.github.com>
2019-01-03 10:57:48 +0100

    Update test_sp_subscribe.sql
    added comments

--- test_sp_subscribe.sql
+++ test_sp_subscribe.sql
@@ -1,3 +1,5 @@
+
+
 -- ADD A NEW CHANNEL WITH SENSORS
 
 --add a new channel with sensors with date
@@ -8,8 +10,7 @@ EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @valid_from_date = '2018-11-20 00:
 EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_name='bla bla',@sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
 
 --add a new channel with sensors with default date and no channel description
-EXEC dbo.sp_prj_subscribe @subscriber_id = 2, @channel_name='bla bla',@sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
-
+EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_name='bla bla',@sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------
@@ -18,33 +19,32 @@ EXEC dbo.sp_prj_subscribe @subscriber_id = 2, @channel_name='bla bla',@sensor_ID
 EXEC dbo.sp_prj_subscribe @channel_id=1, @channel_name ='new', @subscriber_id = 1, @valid_from_date = '2018-11-20 11:00:00 +01:00', @valid_to_date = '2018-12-15 23:59:00 +01:00', 
                           @sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
 
---UPDATE CHANNEL (update valid from and valid to date), with default date
+--UPDATE CHANNEL with default date
 EXEC dbo.sp_prj_subscribe @channel_id=1, @channel_name ='new', @subscriber_id = 1, @sensor_ID_string='4;5;9',@sensor_ID_string_delimeter=';'
 
---UPDATE CHANNEL (update valid from and valid to date), with default date, without @sensor_ID_string, @sensor_ID_string_delimeter
+--UPDATE CHANNEL with default date, without @sensor_ID_string, @sensor_ID_string_delimeter
 EXEC dbo.sp_prj_subscribe @channel_id=1, @channel_name ='new', @subscriber_id = 1
 
---UPDATE CHANNEL (update valid from and valid to date), with default date, without @sensor_ID_string, @sensor_ID_string_delimeter, @channel_name
+--UPDATE CHANNEL with default date, without @sensor_ID_string, @sensor_ID_string_delimeter, @channel_name
+--to update channel at least channel id and subscriber id are necessary!
 EXEC dbo.sp_prj_subscribe @channel_id=1, @subscriber_id = 1
 
 ------------------------------------------------------------------------------------------------------------------------------------------
 
 --ERROR HANDLING:
---add a new channel with sensors with default date and no channel description, without sensor_ID_string_delimeter
-EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_name='bla bla',@sensor_ID_string='4;5;9'
-
---add a new channel with sensors with default date and no channel description, without sensor_ID_string_delimeter and sensor_ID_string
-EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_name='bla bla'
 
 --ERRORNUMBER 50100
 EXEC dbo.sp_prj_subscribe @subscriber_id = 1
+
 --ERRORNUMBER 50101
 EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_id = 1,@valid_from_date = '2018-12-20 11:00:00 +01:00', @valid_to_date = '2020-12-15 23:59:00 +01:00'
+
 --ERRORNUMBER 50102
 EXEC dbo.sp_prj_subscribe @subscriber_id = 99, @channel_id = 1
 
 --ERRORNUMBER 50104
 EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_id = 1,@valid_from_date = '2018-14-20 11:00:00 +01:00', @valid_to_date = '2018-12-15 23:59:00 +01:00'
+
 --ERRORNUMBER 50105
 EXEC dbo.sp_prj_subscribe @subscriber_id = 1, @channel_id = 1,@valid_from_date = '2018-10-20 11:00:00 +01:00', @valid_to_date = '2018-13-15 23:59:00 +01:00'
 
