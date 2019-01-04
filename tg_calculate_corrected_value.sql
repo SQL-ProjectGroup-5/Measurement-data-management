76604fe25e23ff9a890c47e2939c4853c71b5dd4
GZEI <29224009+GZEI@users.noreply.github.com>
2019-01-04 09:36:07 +0100

    Added logging trigger to calculation trigger

--- tg_calculate_corrected_value.sql
+++ tg_calculate_corrected_value.sql
@@ -3,6 +3,7 @@ This Trigger calculates the corrected value of a measurement if not already give
 The given mathematical function is used to generate a SELECT statement which is executed and the resulting value is written to the value_orig field.
 Since this trigger is executed every time a measurement comes in, the runtime is important. To minimize the time needed for execution, only the first value of an insert/update
 is processed (TOP 1 FROM INSERTED). Due to this reason, no entries in the logging table are written.
+-- Returncode 60600: Original Value was changed
 */
 CREATE TRIGGER dbo.tg_calculate_corrected_value_iu ON dbo.measurement
 FOR INSERT, UPDATE
@@ -16,6 +17,8 @@ BEGIN
 	DECLARE @value_orig FLOAT;
 	DECLARE @function NVARCHAR(MAX);
 	DECLARE @parm NVARCHAR(50);
+	DECLARE @oldvalue VARCHAR(10);
+
 	SELECT TOP 1 @sensor = sensor_id, @value_orig = value_orig, @newtimestamp = measure_time, @value_corrected_from_sensor = value_corrected FROM inserted;
 	IF (EXISTS(SELECT * FROM INSERTED) AND EXISTS(SELECT * FROM DELETED) AND UPDATE(value_orig) AND NOT UPDATE(value_corrected))
 	BEGIN
@@ -29,6 +32,8 @@ BEGIN
 		BEGIN
 			UPDATE dbo.measurement SET value_corrected = @value_orig WHERE sensor_ID = @sensor AND measure_time = @newtimestamp;
 		END
+		SELECT TOP 1 @oldvalue = value_orig FROM deleted;
+		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) VALUES (SUSER_NAME(), 'tg_calculate_corrected_value_iu', 50600, 'Original value of measurement was updated Sensor: ' + CONVERT(varchar(10), @sensor) + ' Timestamp: ' + CONVERT(varchar(32),@newtimestamp) + ' Original Value: ' + @oldvalue);
 	END
 	ELSE IF (EXISTS(SELECT * FROM INSERTED))
 	BEGIN
