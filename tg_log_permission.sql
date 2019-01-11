382c7521e4a8a7dd9e4f47f3822bc6892f6ce692
GZEI <29224009+GZEI@users.noreply.github.com>
2019-01-11 20:53:04 +0100

    fixed tg_log_permission

--- tg_log_permission.sql
+++ tg_log_permission.sql
@@ -9,13 +9,12 @@ CREATE TRIGGER dbo.tg_log_permission_iu ON dbo.user_permission
 FOR INSERT, UPDATE
 AS
 BEGIN
-	DECLARE @sensor_id VARCHAR(32);	DECLARE @subscriber_id VARCHAR(32);
-	SELECT TOP 1 @subscriber_id = subscriber_ID, @sensor_id = sensor_ID FROM INSERTED;
+	SET NOCOUNT ON;
 	IF (EXISTS (SELECT * FROM INSERTED) AND EXISTS (SELECT * FROM DELETED))
 	BEGIN
-		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) VALUES (SUSER_NAME(), 'tg_log_permission', 50502, 'Permission was changed subscriber_ID: ' + @subscriber_id + ' sensor_id: ' + @sensor_id);
+		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) SELECT SUSER_NAME(), 'tg_log_permission', 50500, 'Property of permission was updated sensor ID: ' + CAST(i.sensor_ID AS VARCHAR)+ ' subscriber ID: ' + CAST(i.subscriber_ID AS VARCHAR) FROM deleted AS i;
 	END ELSE IF (EXISTS (SELECT * FROM INSERTED))
 	BEGIN
-		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) VALUES (SUSER_NAME(), 'tg_log_permission', 50503, 'New permission added, subscriber_ID' + @subscriber_id + ' sensor_id: ' + @sensor_id);
+		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) SELECT SUSER_NAME(), 'tg_log_permission', 50500, 'New permission was inserted sensor ID: ' + CAST(i.sensor_ID AS VARCHAR)+ ' subscriber ID: ' + CAST(i.subscriber_ID AS VARCHAR) FROM inserted AS i;
 	END
 END
