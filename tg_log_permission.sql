44273826da96865b3bd0431224b0e35bf017a5fc
ANHIP <anna.hipfl99@gmail.com>
2019-01-13 16:23:03 +0100

    Update tg_log_permission.sql

--- tg_log_permission.sql
+++ tg_log_permission.sql
@@ -9,13 +9,7 @@ CREATE TRIGGER dbo.tg_log_permission_iu ON dbo.user_permission
 FOR INSERT, UPDATE
 AS
 BEGIN
-	DECLARE @sensor_id VARCHAR(32);	DECLARE @subscriber_id VARCHAR(32);
-	SELECT TOP 1 @subscriber_id = subscriber_ID, @sensor_id = sensor_ID FROM INSERTED;
-	IF (EXISTS (SELECT * FROM INSERTED) AND EXISTS (SELECT * FROM DELETED))
-	BEGIN
-		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) VALUES (SUSER_NAME(), 'tg_log_permission', 50502, 'Permission was changed subscriber_ID: ' + @subscriber_id + ' sensor_id: ' + @sensor_id);
-	END ELSE IF (EXISTS (SELECT * FROM INSERTED))
-	BEGIN
-		INSERT INTO dbo.logging (causing_user, involved_trigger, resulting_code, resulting_message) VALUES (SUSER_NAME(), 'tg_log_permission', 50503, 'New permission added, subscriber_ID' + @subscriber_id + ' sensor_id: ' + @sensor_id);
-	END
+		SET NOCOUNT ON;
+		INSERT INTO dbo.logging (causing_user,involved_trigger,resulting_code,resulting_message) SELECT SUSER_NAME(), 'tg_log_permission',50502, 'Property of permission was updatet ID: ' + CAST(i.permission_ID AS VARCHAR) + 'name: ' + i.name FROM inserted as i;
+
 END
